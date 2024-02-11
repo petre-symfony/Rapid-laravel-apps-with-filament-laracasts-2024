7231b505a5d7dfe8ce948e7ab515158e96b855aa
petrero <petrero@gmail.com>
2024-02-11 18:10:32 +0200

    1.6 php artisan blueprint:build  So let's go back to the docs for Blueprint and we've got relationships figured out. And so now we really just need to look back at the commands that we have. And the main command we need to worry about is the Blueprint build. So this is what's going to actually build everything that we want. So let's copy that, go back to our terminal. We're going to run artisan blueprint build and you can see what files were created. So we got factories for each of our four models. We got six migrations and then we got models for each of them as well. So if we look, why do we have six when we only did four models?  Well, because it created our many to many pivot tables that we would need when we do that many to many relationship. So it's actually really nice that we have that. Let's go ahead and go into our IDE and take a look at what was generated.

---
+++ database/factories/VenueFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Venue;
+
+class VenueFactory extends Factory {
+	/**
+	 * The name of the factory's corresponding model.
+	 *
+	 * @var string
+	 */
+	protected $model = Venue::class;
+
+	/**
+	 * Define the model's default state.
+	 */
+	public function definition(): array {
+		return [
+			'name' => $this->faker->name(),
+			'city' => $this->faker->city(),
+			'county' => $this->faker->word(),
+			'postal_code' => $this->faker->postcode(),
+		];
+	}
+}
