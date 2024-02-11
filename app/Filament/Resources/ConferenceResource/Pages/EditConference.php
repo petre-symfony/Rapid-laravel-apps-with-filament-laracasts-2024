9a8a9283bf359fe1b75c0da9598122135de0192f
petrero <petrero@gmail.com>
2024-02-11 21:04:58 +0200

    2.9 php artisan make:filament-resource Conference --generate  - Let's bring that back to slate like it was. And if we take a look at the rest of this, there's a bunch of stuff that we're not going to get into right now, but you can see we're still getting a lot of the middleware that you get standard with Laravel. We have authentication.  So now what we want to do is actually build out some of the resources to interact with the models that we created. Let's go back to the filament docs. And this is important using other filament packages. We are, when you install filament with the panel builder, you get every package automatically included. There are no other installation steps. So we are basically done.  We're not worried about deploying to production right now.We don't need the config or any translations. We're done. Filament is installed. Livewire is installed. Alpine is on the front end.  So if we use any Alpine JS, it's already there as well. So now instead of getting started, what we're going to do is jump into the getting started docs here for the resources. And we can create a resource with this command. And what it will do is create the resource and some pages.  And what I want to do is scroll down a little bit further and look at this generate flag. This is so amazing.  So let me show you what happens when I run this command with the generate flag in my terminal. So I'm going to artisan make filament resource. Instead of customer, we're going to do conference. And I'm going to use that generate flag. So before I even go into phpStorm and take a look at what happened, let's go take a look at the browser. So if I refresh my app here, I can now see I have a link on the sidebar that goes to conferences. And when I click here, okay, now I have a page that lists conferences. I don't have any here, but there's a button for a new conference. Let's click here. And all of the fields that I have in my migration that I defined for my model, they're here as well.  So if we are creating Larikon US 2024, this is the best one yet. Let's say it starts. I have a date picker. This is a native browser date picker. And I can also just click in here and type. So let's say it starts at 9 a.m. And let's go to June 7th, 2024. And this will end at, I don't know, 6 p.m. The status is a string.  Let's just put draft in here. The region here is US. And for a venue, I don't have anything to select from here, but I can create it. And now that I've created it, let's go back to our conferences. And I have a table.

---
+++ app/Filament/Resources/ConferenceResource/Pages/EditConference.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\ConferenceResource\Pages;
+
+use App\Filament\Resources\ConferenceResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditConference extends EditRecord {
+	protected static string $resource = ConferenceResource::class;
+
+	protected function getHeaderActions(): array {
+		return [
+			Actions\DeleteAction::make(),
+		];
+	}
+}
