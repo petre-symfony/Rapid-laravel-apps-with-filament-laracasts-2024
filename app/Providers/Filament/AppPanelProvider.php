384975b7c2c10195bec5fa7292620f3f5b0ce80a
petrero <petrero@gmail.com>
2024-02-11 20:42:07 +0200

    2.8 One other thing I want to show, which is kind of cool, if you want to go really crazy with your UI, depends on, you know, who your client is. If you want to actually make the base of the app pretty colorful, you can change gray to a not gray colors. There's nothing that says this string gray has to actually be a gray.  So let's say we did like pink. And let's go take a look at what we have here. That's pretty wild. So I don't want to do this, but if you want to go ahead and play with it

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -27,7 +27,7 @@ class AppPanelProvider extends PanelProvider {
 			->login()
 			->colors([
 				'primary' => Color::Indigo,
-				'gray' => Color::Slate
+				'gray' => Color::Pink
 			])
 			->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
 			->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
