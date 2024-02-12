9b09118a19a9c01c581ec09e125522a227fa529d
petrero <petrero@gmail.com>
2024-02-12 10:18:36 +0200

    3.4 Reformat ConferenceResource file

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -13,93 +13,88 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class ConferenceResource extends Resource
-{
-    protected static ?string $model = Conference::class;
+class ConferenceResource extends Resource {
+	protected static ?string $model = Conference::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+	protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\DateTimePicker::make('start_date')
-                    ->required(),
-                Forms\Components\DateTimePicker::make('end_date')
-                    ->required(),
-                Forms\Components\TextInput::make('status')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('region')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Select::make('venue_id')
-                    ->relationship('venue', 'name'),
-            ]);
-    }
+	public static function form(Form $form): Form {
+		return $form
+			->schema([
+				Forms\Components\TextInput::make('name')
+					->required()
+					->maxLength(255),
+				Forms\Components\TextInput::make('description')
+					->required()
+					->maxLength(255),
+				Forms\Components\DateTimePicker::make('start_date')
+					->required(),
+				Forms\Components\DateTimePicker::make('end_date')
+					->required(),
+				Forms\Components\TextInput::make('status')
+					->required()
+					->maxLength(255),
+				Forms\Components\TextInput::make('region')
+					->required()
+					->maxLength(255),
+				Forms\Components\Select::make('venue_id')
+					->relationship('venue', 'name'),
+			]);
+	}
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('description')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('start_date')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('end_date')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('status')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('region')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('venue.name')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+	public static function table(Table $table): Table {
+		return $table
+			->columns([
+				Tables\Columns\TextColumn::make('name')
+					->searchable(),
+				Tables\Columns\TextColumn::make('description')
+					->searchable(),
+				Tables\Columns\TextColumn::make('start_date')
+					->dateTime()
+					->sortable(),
+				Tables\Columns\TextColumn::make('end_date')
+					->dateTime()
+					->sortable(),
+				Tables\Columns\TextColumn::make('status')
+					->searchable(),
+				Tables\Columns\TextColumn::make('region')
+					->searchable(),
+				Tables\Columns\TextColumn::make('venue.name')
+					->numeric()
+					->sortable(),
+				Tables\Columns\TextColumn::make('created_at')
+					->dateTime()
+					->sortable()
+					->toggleable(isToggledHiddenByDefault: true),
+				Tables\Columns\TextColumn::make('updated_at')
+					->dateTime()
+					->sortable()
+					->toggleable(isToggledHiddenByDefault: true),
+			])
+			->filters([
+				//
+			])
+			->actions([
+				Tables\Actions\EditAction::make(),
+			])
+			->bulkActions([
+				Tables\Actions\BulkActionGroup::make([
+					Tables\Actions\DeleteBulkAction::make(),
+				]),
+			]);
+	}
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
+	public static function getRelations(): array {
+		return [
+			//
+		];
+	}
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListConferences::route('/'),
-            'create' => Pages\CreateConference::route('/create'),
-            'edit' => Pages\EditConference::route('/{record}/edit'),
-        ];
-    }
+	public static function getPages(): array {
+		return [
+			'index' => Pages\ListConferences::route('/'),
+			'create' => Pages\CreateConference::route('/create'),
+			'edit' => Pages\EditConference::route('/{record}/edit'),
+		];
+	}
 }
