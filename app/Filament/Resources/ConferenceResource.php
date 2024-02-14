993e5d0ed1b0393772fb4469ebbd8bc1ecc59a73
petrero <petrero@gmail.com>
2024-02-14 16:14:30 +0200

    5.9 And then let's get rid of this. And our options are just going to be the speaker model. And we're going to pluck the name and the ID. And let's go ahead and go back to the browser. And so now here we can see we only have one speaker.

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -6,6 +6,7 @@ use App\Enums\Region;
 use App\Filament\Resources\ConferenceResource\Pages;
 use App\Filament\Resources\ConferenceResource\RelationManagers;
 use App\Models\Conference;
+use App\Models\Speaker;
 use App\Models\Venue;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -67,10 +68,7 @@ class ConferenceResource extends Resource {
 				Forms\Components\CheckboxList::make('speakers')
 					->relationship('speakers', 'name')
 					->options(
-						SpeakerResource::getModel()::query
-							->withoutGlobalScope(SoftDeletingScope::class)
-							->get()
-							->mapWithKeys(fn ($speaker) => [$speaker->id => $speaker->name])
+						Speaker::all()->pluck('name', 'id')
 					)
 					->required()
 			]);
