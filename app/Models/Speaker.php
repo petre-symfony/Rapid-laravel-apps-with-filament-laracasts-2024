11559fd809a9b96ff099366c44817b7c0560cf85
petrero <petrero@gmail.com>
2024-02-14 15:24:21 +0200

    5.2 Let's go to my speaker resource. We have the form still here on the resource. And I want to put that on the model. So let's cut this here, speaker, get form. And let's go into the speaker model. And at the bottom, we'll add our get form.  And we're going to return what I've got copied here, which is great. And again.

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -16,5 +18,24 @@ class Speaker extends Model {
 
 	public function conferences(): BelongsToMany {
 		return $this->belongsToMany(Conference::class);
+	}
+
+	public static function getForm(): array {
+		return [
+			TextInput::make('name')
+				->required()
+				->maxLength(255),
+			TextInput::make('email')
+				->email()
+				->required()
+				->maxLength(255),
+			Textarea::make('bio')
+				->required()
+				->maxLength(65535)
+				->columnSpanFull(),
+			TextInput::make('twitter_handle')
+				->required()
+				->maxLength(255),
+		];
 	}
 }
