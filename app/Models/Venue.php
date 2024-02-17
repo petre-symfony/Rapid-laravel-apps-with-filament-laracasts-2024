2681f1c0ffbef0771087eaab3b379d6564aa2f1b
petrero <petrero@gmail.com>
2024-02-18 01:46:28 +0200

    8.10 Okay, and then if we go back to the filament docs, it also helps you it links to the Spatie docs where we need to prepare our eloquent model.
     So let's go ahead and look at that really quickly. And we just need to implement this contract and use this trait. So let's actually do that on the venue. Let's say we just want to capture a bunch of pictures of the building.
     So the venue is going to implement has media. And we need to use interacts with media. So here because we're using Spatie's table to store all these images, we don't have to make any changes to the venue model itself. So all we have to do now is go down here. And when we're creating a venue, let's have a Spossy media library file upload input.
     So let's call this images. And this is very similar to the file upload that comes standard, but it does have a few extra options. Let's go back to the browser. And let's go back to the plugin documentation. And it's all pretty straightforward, we can define the collection that we want to use for all of these images.
     And you know, there's some reordering, there's other things you can review these docs, but let's go ahead and implement it really quickly. We have a collection, let's say this is, you know, venue images. In this case, we want multiple. So we just pass in multiple as a method that we can chain on here.
     And these are images as well. I think this is good. So let's go back to the browser. And now let's go to our venues. And when we go to create a new venue, you'll see I have the ability to add multiple images here.
     So let's just, I've got a couple, I'm going to take all three. And when we upload those, they're all going to come through. Let me fill in the rest of this information. And we've got these three. So when I click create, it was created and all three of these were stored.
     If we go into the database, we can see now there is a media table. And if we look at that, we've got all three of our images saved there. If we go back to the browser, they are here on the edit page. If we needed to remove one, we could do that and save the changes.
     And everything's going to work. So you can see how powerful filament is and particularly the forms that we've covered. Now there are a lot more form inputs that are available to you. I'm not going to go over all of these, but this is a really good starting point for you to understand all the different types of things you can do with filling out forms in filament.

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 use App\Enums\Region;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Venue extends Model {
+class Venue extends Model implements HasMedia {
 	use HasFactory;
+	use InteractsWithMedia;
 
 	protected $casts = [
 		'id' => 'integer',
@@ -37,7 +41,11 @@ class Venue extends Model {
 				->maxLength(255),
 			Select::make('region')
 				->enum(Region::class)
-				->options(Region::class)
+				->options(Region::class),
+			SpatieMediaLibraryFileUpload::make('images')
+				->collection('venue-images')
+				->multiple()
+				->image()
 		];
 	}
 }
