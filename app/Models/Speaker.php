037adfcc33f1adbf2a1a4769395c1f4b500d517f
petrero <petrero@gmail.com>
2024-02-18 01:58:57 +0200

    9.1 Table columns  Now that you have a good grasp on Filament Forms, let's take a look at Filament Tables and see how we can visualize the data that we've entered into our database. Now, if you're following along and coding along with me line by line, I did make some changes outside of the video.  So I want to go over those really quickly. We'll start with the speaker model. We actually never added a has many, a speaker has many talks. So that has been added here. And I also just refactored the qualifications that we had into a constant on the class instead of listing it here.  The reason I did that is because I updated the speaker factory a little bit. Number one, to get a random number of qualifications to be set there. And as well, we can add talks when we factory up a speaker.  I also made a few changes to the talks model. So I added three new fields. We have the length of the talk, we have the status of the talk, and whether it's a new talk as a Boolean. So the length and the status are both enums that we talked about before.  So you can see the length here is pretty simple. The status is simple here as well. And if we go to the talk model, you can see we've just added these enums and cast those fields to that enum. So if you're coding along, just go back to the video, pause those sections, add this code, and then we can get started.  You could also reference this commit in the GitHub repository.

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -9,9 +9,23 @@ use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Speaker extends Model {
 	use HasFactory;
+
+	const QUALIFICATIONS = [
+		'business-leader' => 'Business Leader',
+		'charisma' => 'Charismatic Speaker',
+		'first-time' => 'First Time Speaker',
+		'hometown-hero' => 'Hometown Hero',
+		'humanitarian' => 'Works in humanitarian field',
+		'laracasts-contributor' => 'Laracasts Contributor',
+		'twitter-influencer' => 'Large Twitter Following',
+		'youtube-influencer' => 'Large Youtube Following',
+		'open-source' => 'Open Source Creater / Maintainer',
+		'unique-perspective' => 'Unique Perspective'
+	];
 
 	protected $casts = [
 		'id' => 'integer',
@@ -20,6 +34,10 @@ class Speaker extends Model {
 
 	public function conferences(): BelongsToMany {
 		return $this->belongsToMany(Conference::class);
+	}
+
+	public function talks(): HasMany {
+		return $this->hasMany(Talk::class);
 	}
 
 	public static function getForm(): array {
@@ -48,18 +66,7 @@ class Speaker extends Model {
 				->columnSpanFull()
 				->searchable()
 				->bulkToggleable()
-				->options([
-					'business-leader' => 'Business Leader',
-					'charisma' => 'Charismatic Speaker',
-					'first-time' => 'First Time Speaker',
-					'hometown-hero' => 'Hometown Hero',
-					'humanitarian' => 'Works in humanitarian field',
-					'laracasts-contributor' => 'Laracasts Contributor',
-					'twitter-influencer' => 'Large Twitter Following',
-					'youtube-influencer' => 'Large Youtube Following',
-					'open-source' => 'Open Source Creater / Maintainer',
-					'unique-perspective' => 'Unique Perspective'
-				])
+				->options(self::QUALIFICATIONS)
 				->descriptions([
 					'business-leader' => 'Here is a nice long description',
 					'charisma' => 'This is even more information about why you should pick this one'
--- database/factories/SpeakerFactory.php
+++ database/factories/SpeakerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Talk;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Speaker;
@@ -18,12 +19,19 @@ class SpeakerFactory extends Factory {
 	 * Define the model's default state.
 	 */
 	public function definition(): array {
+		$qualificationsCount = $this->faker->numberBetween(0, 10);
+		$qualifications = $this->faker->randomElements(array_keys(Speaker::QUALIFICATIONS), $qualificationsCount);
+
 		return [
 			'name' => $this->faker->name(),
 			'email' => $this->faker->safeEmail(),
 			'bio' => $this->faker->text(),
-			'qualifications' => [],
+			'qualifications' => $qualifications,
 			'twitter_handle' => $this->faker->word(),
 		];
+	}
+
+	public function withTalks(int $count = 1): self {
+		return $this->has(Talk::factory()->count($count), 'talks');
 	}
 }
