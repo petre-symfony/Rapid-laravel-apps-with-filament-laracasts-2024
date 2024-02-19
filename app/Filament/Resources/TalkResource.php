5424db0f7637c6c6f85132e4fcec4748bd8c1760
petrero <petrero@gmail.com>
2024-02-19 21:08:33 +0200

    11.6 I will say this is getting pretty cluttered over here. So Filament gives us a nice way to group all these actions. So let's do an action group. And it's the tables action group. And now this is just going to accept an array of actions here.  So let's just take these two actions that we created. Let's cut them here and paste them into the action group. And if we want to leave the edit action by itself, so we'll do that. Let's make that go first.  So now if we refresh, this is a lot better. We have the edit. And if we want to take action here, we can approve or reject from there.

--- app/Filament/Resources/TalkResource.php
+++ app/Filament/Resources/TalkResource.php
@@ -95,31 +95,33 @@ class TalkResource extends Resource {
 			->actions([
 				Tables\Actions\EditAction::make()
 					->slideOver(),
-				Tables\Actions\Action::make('approve')
-					->icon('heroicon-o-check-circle')
-					->color('success')
-					->action(function (Talk $record) {
-						$record->approved();
-					})
-					->after(function () {
-						Notification::make()->success()->title('This talk was approved')
-							->duration(1000)
-							->body('The speaker has been notified and the talk has been added to the conference schedule')
-							->send();
-					}),
-				Tables\Actions\Action::make('reject')
-					->icon('heroicon-o-no-symbol')
-					->color('danger')
-					->requiresConfirmation()
-					->action(function (Talk $record) {
-						$record->reject();
-					})
-					->after(function () {
-						Notification::make()->danger()->title('This talk was rejected')
-							->duration(1000)
-							->body('The speaker has been notified')
-							->send();
-					})
+				Tables\Actions\ActionGroup::make([
+					Tables\Actions\Action::make('approve')
+						->icon('heroicon-o-check-circle')
+						->color('success')
+						->action(function (Talk $record) {
+							$record->approved();
+						})
+						->after(function () {
+							Notification::make()->success()->title('This talk was approved')
+								->duration(1000)
+								->body('The speaker has been notified and the talk has been added to the conference schedule')
+								->send();
+						}),
+					Tables\Actions\Action::make('reject')
+						->icon('heroicon-o-no-symbol')
+						->color('danger')
+						->requiresConfirmation()
+						->action(function (Talk $record) {
+							$record->reject();
+						})
+						->after(function () {
+							Notification::make()->danger()->title('This talk was rejected')
+								->duration(1000)
+								->body('The speaker has been notified')
+								->send();
+						})
+				]),
 			])
 			->bulkActions([
 				Tables\Actions\BulkActionGroup::make([
