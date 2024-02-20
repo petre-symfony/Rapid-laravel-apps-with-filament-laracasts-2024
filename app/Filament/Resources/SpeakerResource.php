e78a7f7e23fc095694e573d0190a64caf614cc1e
petrero <petrero@gmail.com>
2024-02-20 19:47:47 +0200

    12.4 I like to have a dedicated view, and then click a button to edit. So we can do that by going to our view page. But instead of using this, we can define a different schema for how we want to view the page, which is called an info list. So let's go into our resource. And we can add another method here.  So just go ahead and type info list. And you should have your IDE if you're using it, pull this up. And so for an info list, the info list is passed into the method here, and we're just going to return it.  But before we do, we're going to define the schema. So just like we define schema for forms, and groups and sections within forms, we're going to do the same thing on the info list. So really quickly, we do have those same sections that we have on forms, we have them as info lists. But make sure again, you're always looking at the namespace, we need the info list section. So let's do this.  And let's just call it personal information. And now we define the schema. So instead of text inputs, we're looking for text entries. So let's do a text entry. And then we're going to name this the name of the database field, just like we would on a text input. And let's just see what this looks like. So we go to our browser, hit refresh.  So here you can get a little sense of what this is going to look like as we continue to build it out. So let's go back into our info list. And let's add something else. So we've got a text entry for the email, there's also a Twitter handle.

--- app/Filament/Resources/SpeakerResource.php
+++ app/Filament/Resources/SpeakerResource.php
@@ -7,6 +7,9 @@ use App\Filament\Resources\SpeakerResource\RelationManagers;
 use App\Models\Speaker;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -52,6 +55,18 @@ class SpeakerResource extends Resource {
 				Tables\Actions\BulkActionGroup::make([
 					Tables\Actions\DeleteBulkAction::make(),
 				]),
+			]);
+	}
+
+	public static function infolist(Infolist $infolist): Infolist {
+		return $infolist
+			->schema([
+				Section::make('Personal Information')
+					->schema([
+						TextEntry::make('name'),
+						TextEntry::make('email'),
+						TextEntry::make('twitter_handle')
+					])
 			]);
 	}
 
