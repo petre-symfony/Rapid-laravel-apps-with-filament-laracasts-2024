a1b7801bd8f163573b6e15b47db85b540d675218
petrero <petrero@gmail.com>
2024-02-20 21:51:44 +0200

    12.9 Now one thing what I actually like to do is I don't really like being able to edit directly from the table. I think it makes a lot more sense to just view the record. And then if you want to edit, let's have the slide over from the view page.
     So that's up to you. But let's implement that here. So if we are on our speaker resource, let's go ahead and get rid of the edit action.
     And then within the view action, or actually not in the view action, we go down to the pages and we need to go to the view speaker page.
     And here, this is a pretty simple class, but we can add a method to get the header actions. And so here, we can just return an edit action, which in this case, we're not in a table. So we'll just do the regular edit action, make, and then we just need to pass in the form. And the form here is, of course, the speaker get form.
     And so now if we go to our browser and go back to the speaker, so let's view this. And I'm getting an error here because this accepts an array and I just passed a single one. So let's do that. All right, and go back to the page. And here we go. So now we have an edit button.

--- app/Filament/Resources/SpeakerResource/Pages/ViewSpeaker.php
+++ app/Filament/Resources/SpeakerResource/Pages/ViewSpeaker.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\SpeakerResource\Pages;
 
 use App\Filament\Resources\SpeakerResource;
+use App\Models\Speaker;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewSpeaker extends ViewRecord {
 	protected static string $resource = SpeakerResource::class;
+
+	protected function getHeaderActions(): array {
+		return [
+			Actions\EditAction::make()
+				->form(Speaker::getForm())
+		];
+	}
 }
