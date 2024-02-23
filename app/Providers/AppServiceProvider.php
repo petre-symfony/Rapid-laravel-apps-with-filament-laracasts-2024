42e4db6d9b297c6202c866680093c3bbb3948bc0
petrero <petrero@gmail.com>
2024-02-24 00:38:10 +0200

    13.6 Now, as I've said in other videos, when I am doing something, especially filling out a form, I don't like it. As a modal, I really like that as a slide over. And I've had to continually add that slide over method and chain it to the different actions. I don't want to do that anymore. So there is a way to make that a little more simple. And to do that, we need to go into any app service provider, I just like to use the one that comes standard with Laravel. And in the boot method, we have the ability to actually set global defaults.
     So let's go back to our relation manager and take a look in the table. This is the tables create action. So what I want to do is actually get this action itself, go back to my app service provider, paste it in here, I'll actually import the namespace, but I want to make sure it's the tables actions. There we go. And I'm going to call a static method called configure using. And what this is going to allow me to do is pass in the action, and then set some defaults that I like to use always.
     So in this case, it's really simple, I'll just return action slide over. And that's it. Every time Laravel boots up, it's going to run through here. And by default, every create action on a table is going to use a slide over. Now I could override it, if for some reason I want to use the modal on that particular create action. But if we go back to the browser here, and we do a new talk, now we're going to get a slide over every time that is useful for a bunch of stuff.
     If we wanted to configure which of these different things in the rich editor we want to see, you don't want to have to do that every time you add a rich editor. So you do that in the app service provider, and it will be consistent across your entire app.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Actions\CreateAction;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,5 +19,9 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function boot(): void {
 		Model::unguard();
+
+		CreateAction::configureUsing(function ($action) {
+			return $action->slideOver();
+		});
 	}
 }
