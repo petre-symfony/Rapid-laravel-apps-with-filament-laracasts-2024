e238407dc77d4ed269ba8e63ac0b162d61eaf52e
petrero <petrero@gmail.com>
2024-02-24 11:30:55 +0200

    14.3 php artisan make:livewire  OK, and now I should be ready to go ahead and use forms on the front end, which that's what we need.
     We need to have a form where we can take in the attendee information. So the other thing I need to do now is generate my Livewire component.
     So let's do artisan make Livewire. This is standard Livewire and we're going to call this the conference sign up page. And then we need to do what we would normally do in a Livewire app.

---
+++ app/Livewire/ConferenceSignUpPage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ConferenceSignUpPage extends Component {
+	public function render() {
+		return view('livewire.conference-sign-up-page');
+	}
+}
