a4ed25726bd3ef585ccef82dac7f7a91cbe8fc02
petrero <petrero@gmail.com>
2024-02-24 16:39:58 +0200

    16.1  Notifications  All right, the last of the filament packages that we haven't explicitly covered, although we've been using it throughout the course, is the notifications package. Now this is a really nice, easy way to send notifications to your users, and is actually one of the best ways to get into Filament  if you have an existing app, and you just wanna add these notifications in, and then maybe work your way into some of the other packages as well. So let's use this widget that we just created for the dashboard, and let's just use some of the things we've learned, make sure we can add an action to be able to send a notification, and then let's just play with that class a little bit. So we go back into phpStorm. We're in our test chart, and so again, a widget, especially a custom widget, is really just a Livewire component that we're gonna be using. So let's create an action, and implement this action in this blade view. So instead of hello, let's have this call notification. So this is an action, and we're gonna create that in our class. So we need a public function for call notification. It's going to return an action, and in order to use actions in a Livewire component, we have to implement hasActions and hasForms, and we need to use interactsWithActions and interactsWithForms, right? And the hasActions wasn't imported, let's do that. And now we should be able to really simply, again, this is just standard filament using the actions. We would return an action. We're going to make it call notification.  What we're doing now is customizing the look and feel, so let's make it a button. Let's make the color warning. The label is going to be send a notification, and then we pass in what we actually want it to do in the action method. So we have a closure here, and this is gonna be really simple. We just want to pass in the filament notification class. We're going to make one. It's going to be a warning, and let's give it a title and a body. And of course, we have to send notifications. It's something I regularly forget. If you're wondering why your notification's not actually showing up, it's probably because you haven't added the send method to the end of it. So let's go back to the browser.  Let's refresh. Okay, so now we have a Livewire widget, and the widget has an action in it, and if we click send a notification, we get this notification here. So it's pretty simple, pretty straightforward.

--- app/Filament/Widgets/TestChart.php
+++ app/Filament/Widgets/TestChart.php
@@ -2,10 +2,33 @@
 
 namespace App\Filament\Widgets;
 
+use Filament\Actions\Action;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Notifications\Notification;
 use Filament\Widgets\Widget;
 
-class TestChart extends Widget {
+class TestChart extends Widget implements HasActions, HasForms {
+	use InteractsWithForms;
+	use InteractsWithActions;
+
 	protected int | string | array $columnSpan = 'full';
 
 	protected static string $view = 'filament.widgets.test-chart';
+
+	public function callNotification(): Action {
+		return Action::make('callNotification')
+			->button()
+			->color('warning')
+			->label('Send a notification')
+			->action(function () {
+				Notification::make()
+					->warning()
+					->title('You sent a notification')
+					->body('This is a test')
+					->send();
+			});
+	}
 }
