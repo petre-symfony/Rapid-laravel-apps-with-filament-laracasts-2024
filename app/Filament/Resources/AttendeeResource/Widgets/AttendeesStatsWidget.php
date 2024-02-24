5b2534e78e3f0e18fdc6e153cfbd26fc45cdab59
petrero <petrero@gmail.com>
2024-02-24 13:08:45 +0200

    15.5 Now let's say some people are canceling and the numbers go down. We could go back to 411 and save this. And if we refresh, you can see the chart starts trending down.

--- app/Filament/Resources/AttendeeResource/Widgets/AttendeesStatsWidget.php
+++ app/Filament/Resources/AttendeeResource/Widgets/AttendeesStatsWidget.php
@@ -13,7 +13,7 @@ class AttendeesStatsWidget extends BaseWidget {
 				->description('Total number of attendees')
 				->descriptionIcon('heroicon-o-user-group')
 				->color('success')
-				->chart([1, 2, 3, 4,5])
+				->chart([1, 2, 3, 4, 5, 4, 1, 1])
 			,
 			Stat::make('Total Revenue', Attendee::sum('ticket_cost') / 100)
 		];
